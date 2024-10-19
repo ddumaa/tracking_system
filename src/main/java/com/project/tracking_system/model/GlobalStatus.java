09e68a7d2d52cd80382822d2e5ea542363b1c1c7
ddumaa <dimaduma62@gmail.com>
2024-10-19 16:28:24 +0300

    fix status

--- src/main/java/com/project/tracking_system/model/GlobalStatus.java
+++ src/main/java/com/project/tracking_system/model/GlobalStatus.java
@@ -1,12 +1,19 @@
 package com.project.tracking_system.model;
 
+import lombok.Getter;
+
+@Getter
 public enum GlobalStatus {
 
     DELIVERED("Вручена", "<i class=\"bi bi-check2-circle\" style=\"font-size: 2rem; color: #008000\"></i>"),
     WAITING_FOR_CUSTOMER("Ожидает клиента", "<i class=\"bi bi-clock-history\" style=\"font-size: 2rem; color: #fff200\"></i>"),
     IN_TRANSIT("В пути к клиенту", "<i class=\"bi bi-truck\" style=\"font-size: 2rem; color: #0000FF\"></i>"),
+    IN_TRANSIT_TO_SORTING("В пути на сортировку", "<i class=\"bi bi-truck\" style=\"font-size: 2rem; color: #0000FF\"></i>"),
+    IN_TRANSIT_TO_DELIVERY("В пути к ОПС назначения", "<i class=\"bi bi-truck\" style=\"font-size: 2rem; color: #0000FF\"></i>"),
     CUSTOMER_NOT_PICKING_UP("Клиент не забирает посылку", "<i class=\"bi bi-clock-history\" style=\"font-size: 2rem; color: #ff7300\"></i>"),
     RETURN_IN_PROGRESS("Возврат в пути", "<i class=\"bi bi-truck\" style=\"font-size: 2rem; color: #FF0000; transform: scaleX(-1)\"></i>"),
+    RETURN_IN_TRANSIT_TO_SORTING("Возврат на сортировку", "<i class=\"bi bi-truck\" style=\"font-size: 2rem; color: #FF0000; transform: scaleX(-1)\"></i>"),
+    RETURN_IN_TRANSIT_TO_SENDER("Возврат в пути к отправителю", "<i class=\"bi bi-truck\" style=\"font-size: 2rem; color: #FF0000; transform: scaleX(-1)\"></i>"),
     RETURNED_TO_SENDER("Возврат забран", "<i class=\"bi bi-check2-circle\" style=\"font-size: 2rem; color: #FF0000\"></i>"),
     REGISTERED("Заявка зарегистрирована", "<i class=\"bi bi-file-earmark-text\" style=\"font-size: 2rem; color: #007bff\"></i>");
 
@@ -16,14 +23,6 @@ public enum GlobalStatus {
     GlobalStatus(String description, String iconHtml) {
         this.description = description;
         this.iconHtml = iconHtml;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getIconHtml() {
-        return iconHtml;
     }
 
 }
--- src/main/java/com/project/tracking_system/service/StatusTrackService.java
+++ src/main/java/com/project/tracking_system/service/StatusTrackService.java
@@ -17,16 +17,27 @@ public class StatusTrackService {
         statusPatterns.put(Pattern.compile("^Почтовое отправление прибыло на ОПС выдачи|^Добрый день\\. Срок бесплатного хранения|" +
                 "^Поступило в учреждение доставки"), GlobalStatus.WAITING_FOR_CUSTOMER);
         statusPatterns.put(Pattern.compile("^Почтовое отправление принято на ОПС|" +
-                "^Оплачено на ОПС|^Почтовое отправление подготовлено в ОПС к доставке|^Почтовое отправление прибыло на сортировочный пункт|" +
-                "^Почтовое отправление подготовлено в сортировочном пункте к доставке на ОПС назначения|^Отправлено|^Принято от отправителя|" +
+                "^Оплачено на ОПС|^Отправлено|^Принято от отправителя|" +
                 "^Поступило в обработку"), GlobalStatus.IN_TRANSIT);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление подготовлено в ОПС к доставке на сортировочный пункт$"),
+                GlobalStatus.IN_TRANSIT_TO_SORTING);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление прибыло на сортировочный пункт$"),
+                GlobalStatus.IN_TRANSIT_TO_SORTING);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление подготовлено в сортировочном пункте к доставке на ОПС назначения$"),
+                GlobalStatus.IN_TRANSIT_TO_DELIVERY);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление готово к возврату"), GlobalStatus.RETURN_IN_PROGRESS);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление подготовлено в ОПС к доставке на сортировочный пункт.*возврат.*$"),
+                GlobalStatus.RETURN_IN_TRANSIT_TO_SORTING);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление прибыло на сортировочный пункт.*возврат.*$"),
+                GlobalStatus.RETURN_IN_TRANSIT_TO_SORTING);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление подготовлено в сортировочном пункте к доставке на ОПС отправителя$"),
+                GlobalStatus.RETURN_IN_TRANSIT_TO_SENDER);
+        statusPatterns.put(Pattern.compile("^Почтовое отправление возвращено отправителю$"), GlobalStatus.RETURNED_TO_SENDER);
         statusPatterns.put(Pattern.compile("^Заявка на почтовое отправление зарегистрирована$"), GlobalStatus.REGISTERED);
         statusPatterns.put(Pattern.compile("^Добрый день\\. Отправление [A-Z0-9]+ не востребовано получателем.*|" +
-                "^Отправление с [0-9]{2}\\.[0-9]{2}\\.[0-9]{4} ожидает вручения в Отделение.*|" +
-                "^Добрый день\\. Ваше почтовое отправление [A-Z0-9]+ будет возвращено отправителю через 10 дней\\."), GlobalStatus.CUSTOMER_NOT_PICKING_UP);
-        statusPatterns.put(Pattern.compile("^Почтовое отправление готово к возврату|" +
-                "^Почтовое отправление подготовлено в ОПС к доставке на сортировочный пункт"), GlobalStatus.RETURN_IN_PROGRESS);
-        statusPatterns.put(Pattern.compile("^Почтовое отправление возвращено отправителю$"), GlobalStatus.RETURNED_TO_SENDER);
+                        "^Отправление с [0-9]{2}\\.[0-9]{2}\\.[0-9]{4} ожидает вручения в Отделение.*|" +
+                        "^Добрый день\\. Ваше почтовое отправление [A-Z0-9]+ будет возвращено отправителю через 10 дней\\."),
+                GlobalStatus.CUSTOMER_NOT_PICKING_UP);
     }
 
     public String setStatus(String statusTrack) {
