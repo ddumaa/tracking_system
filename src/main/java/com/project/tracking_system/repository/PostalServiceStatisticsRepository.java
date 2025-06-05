da3850e3a6ab3cee4f8f08d9fd90f9005a0f3928
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-06-05 16:13:26 +0300

    Add missing List import

--- src/main/java/com/project/tracking_system/repository/PostalServiceStatisticsRepository.java
+++ src/main/java/com/project/tracking_system/repository/PostalServiceStatisticsRepository.java
@@ -4,6 +4,7 @@ import com.project.tracking_system.entity.PostalServiceStatistics;
 import com.project.tracking_system.entity.PostalServiceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostalServiceStatisticsRepository extends JpaRepository<PostalServiceStatistics, Long> {
