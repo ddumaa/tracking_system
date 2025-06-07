57fdf4f2b9c9de8bc218d6888f60a2ff5b09c204
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-06-08 00:48:19 +0300

    Add bulk retrieval methods for store statistics

--- src/main/java/com/project/tracking_system/repository/StoreMonthlyStatisticsRepository.java
+++ src/main/java/com/project/tracking_system/repository/StoreMonthlyStatisticsRepository.java
@@ -3,6 +3,7 @@ package com.project.tracking_system.repository;
 import com.project.tracking_system.entity.StoreMonthlyStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -11,4 +12,14 @@ import java.util.Optional;
 public interface StoreMonthlyStatisticsRepository extends JpaRepository<StoreMonthlyStatistics, Long> {
 
     Optional<StoreMonthlyStatistics> findByStoreIdAndPeriodYearAndPeriodNumber(Long storeId, int periodYear, int periodNumber);
+
+    /**
+     * Find statistics for multiple stores for the given month of a year.
+     *
+     * @param storeIds    list of store identifiers
+     * @param periodYear  year of the month
+     * @param periodNumber number of the month (1-12)
+     * @return list of monthly statistics, one per store if present
+     */
+    List<StoreMonthlyStatistics> findByStoreIdInAndPeriodYearAndPeriodNumber(List<Long> storeIds, int periodYear, int periodNumber);
 }
--- src/main/java/com/project/tracking_system/repository/StoreWeeklyStatisticsRepository.java
+++ src/main/java/com/project/tracking_system/repository/StoreWeeklyStatisticsRepository.java
@@ -3,6 +3,7 @@ package com.project.tracking_system.repository;
 import com.project.tracking_system.entity.StoreWeeklyStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -11,4 +12,14 @@ import java.util.Optional;
 public interface StoreWeeklyStatisticsRepository extends JpaRepository<StoreWeeklyStatistics, Long> {
 
     Optional<StoreWeeklyStatistics> findByStoreIdAndPeriodYearAndPeriodNumber(Long storeId, int periodYear, int periodNumber);
+
+    /**
+     * Find statistics for multiple stores for the given week of a year.
+     *
+     * @param storeIds    list of store identifiers
+     * @param periodYear  year of the week
+     * @param periodNumber number of the week within the year
+     * @return list of weekly statistics, one per store if present
+     */
+    List<StoreWeeklyStatistics> findByStoreIdInAndPeriodYearAndPeriodNumber(List<Long> storeIds, int periodYear, int periodNumber);
 }
