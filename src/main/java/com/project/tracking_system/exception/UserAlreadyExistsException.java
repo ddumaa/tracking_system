38f98034274f08d70c91f108ff2f3d6ae2003bf2
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-06-08 23:17:18 +0300

    Перевести JavaDoc исключений на русский

--- src/main/java/com/project/tracking_system/exception/UserAlreadyExistsException.java
+++ src/main/java/com/project/tracking_system/exception/UserAlreadyExistsException.java
@@ -1,6 +1,16 @@
 package com.project.tracking_system.exception;
 
+/**
+ * Исключение, возникающее при попытке зарегистрировать пользователя,
+ * который уже существует в системе.
+ */
 public class UserAlreadyExistsException extends RuntimeException {
+
+    /**
+     * Создаёт исключение с указанным сообщением.
+     *
+     * @param message описание ошибки
+     */
     public UserAlreadyExistsException(String message) {
         super(message);
     }
