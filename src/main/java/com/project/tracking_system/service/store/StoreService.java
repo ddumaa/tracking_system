904a077883a14892dc6c97e08fafdf2060b6b48e
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-06-14 23:50:17 +0300

    Fix migration CTE and cleanup README

--- src/main/java/com/project/tracking_system/service/store/StoreService.java
+++ src/main/java/com/project/tracking_system/service/store/StoreService.java
@@ -95,26 +95,36 @@ public class StoreService {
         Store savedStore = storeRepository.save(store);
         log.info("Магазин '{}' создан с ID={}", savedStore.getName(), savedStore.getId());
 
-        // Создаём пустую статистику для нового магазина
-        StoreStatistics statistics = new StoreStatistics();
-        statistics.setStore(savedStore);
-        statistics.setTotalSent(0);
-        statistics.setTotalDelivered(0);
-        statistics.setTotalReturned(0);
-        statistics.setSumDeliveryDays(BigDecimal.ZERO);
-        statistics.setSumPickupDays(BigDecimal.ZERO);
-        statistics.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
+        // Создаём пустую статистику для нового магазина если её ещё нет
+        if (storeAnalyticsRepository.findByStoreId(savedStore.getId()).isEmpty()) {
+            StoreStatistics statistics = new StoreStatistics();
+            statistics.setStore(savedStore);
+            statistics.setTotalSent(0);
+            statistics.setTotalDelivered(0);
+            statistics.setTotalReturned(0);
+            statistics.setSumDeliveryDays(BigDecimal.ZERO);
+            statistics.setSumPickupDays(BigDecimal.ZERO);
+            statistics.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
 
-        storeAnalyticsRepository.save(statistics);
-        log.info("Создана пустая статистика для магазина ID={}", savedStore.getId());
+            storeAnalyticsRepository.save(statistics);
+            log.info("Создана пустая статистика для магазина ID={}", savedStore.getId());
+        } else {
+            log.warn("Статистика для магазина ID={} уже существует", savedStore.getId());
+        }
 
         // Создаём пустую статистику для каждой почтовой службы
         for (PostalServiceType type : PostalServiceType.values()) {
-            PostalServiceStatistics psStats = new PostalServiceStatistics();
-            psStats.setStore(savedStore);
-            psStats.setPostalServiceType(type);
-            psStats.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-            postalServiceStatisticsRepository.save(psStats);
+            if (postalServiceStatisticsRepository
+                    .findByStoreIdAndPostalServiceType(savedStore.getId(), type)
+                    .isEmpty()) {
+                PostalServiceStatistics psStats = new PostalServiceStatistics();
+                psStats.setStore(savedStore);
+                psStats.setPostalServiceType(type);
+                psStats.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
+                postalServiceStatisticsRepository.save(psStats);
+            } else {
+                log.warn("Статистика {} уже существует для магазина ID={}", type, savedStore.getId());
+            }
         }
 
         webSocketController.sendUpdateStatus(userId, "Магазин '" + storeName + "' добавлен!", true);
