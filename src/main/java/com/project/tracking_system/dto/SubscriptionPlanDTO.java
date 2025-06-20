d525b2c8c49d65e8444f4b321a4842c10c158d50
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-06-20 15:22:14 +0300

    feat: add subscription plan management

---
+++ src/main/java/com/project/tracking_system/dto/SubscriptionPlanDTO.java
@@ -0,0 +1,21 @@
+package com.project.tracking_system.dto;
+
+import com.project.tracking_system.entity.SubscriptionCode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO для работы с тарифными планами.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionPlanDTO {
+    private SubscriptionCode code;
+    private Integer maxTracksPerFile;
+    private Integer maxSavedTracks;
+    private Integer maxTrackUpdates;
+    private boolean allowBulkUpdate;
+    private Integer maxStores;
+}
