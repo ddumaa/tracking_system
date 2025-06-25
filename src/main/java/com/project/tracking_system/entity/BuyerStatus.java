4548fe2f82d1c06e9049c1f8fd760c01b2b9f7c7
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-06-26 00:37:28 +0300

    Add display name for BuyerStatus and use in profile template

--- src/main/java/com/project/tracking_system/entity/BuyerStatus.java
+++ src/main/java/com/project/tracking_system/entity/BuyerStatus.java
@@ -1,26 +1,47 @@
 package com.project.tracking_system.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 /**
- * @author Dmitriy Anisimov
- * @date 19.06.2025
+ * Статус посылки, предназначенный для отображения покупателю.
+ * Содержит шаблон сообщения и читабельное название на русском языке.
  */
-@Getter
-@AllArgsConstructor
 public enum BuyerStatus {
 
-    REGISTERED("Ваш заказ %s из магазина %s зарегистрирован и скоро будет отправлен."),
-    IN_TRANSIT("Посылка %s из магазина %s находится в пути."),
-    WAITING("Посылка %s из магазина %s прибыла и ждёт вас в пункте выдачи."),
-    DELIVERED("Посылка %s из магазина %s получена. Спасибо за покупку!"),
-    RETURNED("Посылка {track} из магазина {store} уже возвращается отправителю.");
+    /** Заказ зарегистрирован. */
+    REGISTERED("Зарегистрирован", "Ваш заказ %s из магазина %s зарегистрирован и скоро будет отправлен."),
+    /** Посылка находится в пути. */
+    IN_TRANSIT("В пути", "Посылка %s из магазина %s находится в пути."),
+    /** Посылка ожидает получения. */
+    WAITING("Ожидает получения", "Посылка %s из магазина %s прибыла и ждёт вас в пункте выдачи."),
+    /** Посылка получена покупателем. */
+    DELIVERED("Получена", "Посылка %s из магазина %s получена. Спасибо за покупку!"),
+    /** Посылка возвращается отправителю. */
+    RETURNED("Возврат", "Посылка %s из магазина %s уже возвращается отправителю.");
 
+    private final String displayName;
     private final String messageTemplate;
 
+    BuyerStatus(String displayName, String messageTemplate) {
+        this.displayName = displayName;
+        this.messageTemplate = messageTemplate;
+    }
+
+    /**
+     * Возвращает русскоязычное название статуса.
+     *
+     * @return читаемое название статуса
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    /**
+     * Формирует сообщение на основе переданных данных.
+     *
+     * @param track трек-номер посылки
+     * @param store название магазина
+     * @return сформированное сообщение
+     */
     public String formatMessage(String track, String store) {
         return String.format(messageTemplate, track, store);
     }
-
 }
