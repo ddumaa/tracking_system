06bd548117a5c638db9876f9df078979ebbac724
ddumaa <dimaduma62@gmail.com>
2025-07-07 01:46:54 +0300

    fix

--- src/main/java/com/project/tracking_system/configuration/SecurityConfiguration.java
+++ src/main/java/com/project/tracking_system/configuration/SecurityConfiguration.java
@@ -69,6 +69,9 @@ public class SecurityConfiguration {
             log.warn("Используется значение по умолчанию для security.remember-me-key. Задайте уникальное значение в application.properties!");
         }
         http
+                .requiresChannel(channel -> channel
+                        .anyRequest().requiresSecure()
+                )
                 .addFilterBefore(cspNonceFilter, SecurityContextPersistenceFilter.class)
                 .headers(h -> h
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
