2fb9a04b91bb9631d621bbb793a426f6263cb5cd
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-07-19 18:21:13 +0300

    refactor: extract track statistics updater

--- src/main/java/com/project/tracking_system/service/track/TrackProcessingService.java
+++ src/main/java/com/project/tracking_system/service/track/TrackProcessingService.java
@@ -3,6 +3,7 @@ package com.project.tracking_system.service.track;
 import com.project.tracking_system.dto.TrackInfoListDTO;
 import com.project.tracking_system.entity.*;
 import com.project.tracking_system.repository.*;
+import com.project.tracking_system.service.analytics.TrackStatisticsUpdater;
 import com.project.tracking_system.service.SubscriptionService;
 import com.project.tracking_system.service.analytics.DeliveryHistoryService;
 import com.project.tracking_system.service.customer.CustomerService;
@@ -38,10 +39,7 @@ public class TrackProcessingService {
     private final StoreRepository storeRepository;
     private final UserRepository userRepository;
     private final TrackParcelRepository trackParcelRepository;
-    private final StoreAnalyticsRepository storeAnalyticsRepository;
-    private final PostalServiceStatisticsRepository postalServiceStatisticsRepository;
-    private final StoreDailyStatisticsRepository storeDailyStatisticsRepository;
-    private final PostalServiceDailyStatisticsRepository postalServiceDailyStatisticsRepository;
+    private final TrackStatisticsUpdater trackStatisticsUpdater;
 
     /**
      * Обрабатывает номер посылки: получает информацию и при необходимости сохраняет её.
@@ -200,124 +198,7 @@ public class TrackProcessingService {
             customerStatsService.incrementSent(customer);
         }
 
-        boolean storeChanged = !isNewParcel && previousStoreId != null && !previousStoreId.equals(storeId);
-
-        PostalServiceType serviceType = typeDefinitionTrackPostService.detectPostalService(number);
-
-        // Инкрементируем статистику нового магазина при добавлении новой посылки или смене магазина
-        if (isNewParcel || storeChanged) {
-            StoreStatistics statistics = storeAnalyticsRepository.findByStoreId(storeId)
-                    .orElseThrow(() -> new IllegalStateException("Статистика не найдена"));
-
-            // Атомарное обновление счётчика отправлений
-            int updated = storeAnalyticsRepository.incrementTotalSent(storeId, 1);
-            if (updated == 0) {
-                statistics.setTotalSent(statistics.getTotalSent() + 1);
-                statistics.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                storeAnalyticsRepository.save(statistics);
-            }
-
-            // Статистика почтовой службы (пропустить UNKNOWN)
-            if (serviceType != PostalServiceType.UNKNOWN) {
-                int psUpdated = postalServiceStatisticsRepository.incrementTotalSent(storeId, serviceType, 1);
-                if (psUpdated == 0) {
-                    PostalServiceStatistics psStats = postalServiceStatisticsRepository
-                            .findByStoreIdAndPostalServiceType(storeId, serviceType)
-                            .orElseGet(() -> {
-                                PostalServiceStatistics s = new PostalServiceStatistics();
-                                s.setStore(statistics.getStore());
-                                s.setPostalServiceType(serviceType);
-                                return s;
-                            });
-                    psStats.setTotalSent(psStats.getTotalSent() + 1);
-                    psStats.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                    postalServiceStatisticsRepository.save(psStats);
-                }
-            } else {
-                log.warn("⛔ Пропуск обновления аналитики для UNKNOWN службы: {}", number);
-            }
-
-            // Ежедневная статистика магазина
-            LocalDate day = zonedDateTime.toLocalDate();
-            int dailyUpdated = storeDailyStatisticsRepository.incrementSent(storeId, day, 1);
-            if (dailyUpdated == 0) {
-                StoreDailyStatistics daily = storeDailyStatisticsRepository
-                        .findByStoreIdAndDate(storeId, day)
-                        .orElseGet(() -> {
-                            StoreDailyStatistics d = new StoreDailyStatistics();
-                            d.setStore(statistics.getStore());
-                            d.setDate(day);
-                            return d;
-                        });
-                daily.setSent(daily.getSent() + 1);
-                daily.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                storeDailyStatisticsRepository.save(daily);
-            }
-
-            // Ежедневная статистика почтовой службы
-            if (serviceType != PostalServiceType.UNKNOWN) {
-                int psdUpdated = postalServiceDailyStatisticsRepository.incrementSent(storeId, serviceType, day, 1);
-                if (psdUpdated == 0) {
-                    PostalServiceDailyStatistics psDaily = postalServiceDailyStatisticsRepository
-                            .findByStoreIdAndPostalServiceTypeAndDate(storeId, serviceType, day)
-                            .orElseGet(() -> {
-                                PostalServiceDailyStatistics d = new PostalServiceDailyStatistics();
-                                d.setStore(statistics.getStore());
-                                d.setPostalServiceType(serviceType);
-                                d.setDate(day);
-                                return d;
-                            });
-                    psDaily.setSent(psDaily.getSent() + 1);
-                    psDaily.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                    postalServiceDailyStatisticsRepository.save(psDaily);
-                }
-            }
-        }
-
-        // Декрементируем статистику старого магазина, если посылка перенесена
-        if (storeChanged) {
-            StoreStatistics oldStats = storeAnalyticsRepository.findByStoreId(previousStoreId)
-                    .orElseThrow(() -> new IllegalStateException("Статистика не найдена"));
-            if (oldStats.getTotalSent() > 0) {
-                oldStats.setTotalSent(oldStats.getTotalSent() - 1);
-                oldStats.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                storeAnalyticsRepository.save(oldStats);
-            }
-
-            if (serviceType != PostalServiceType.UNKNOWN) {
-                PostalServiceStatistics oldPs = postalServiceStatisticsRepository
-                        .findByStoreIdAndPostalServiceType(previousStoreId, serviceType)
-                        .orElse(null);
-                if (oldPs != null && oldPs.getTotalSent() > 0) {
-                    oldPs.setTotalSent(oldPs.getTotalSent() - 1);
-                    oldPs.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                    postalServiceStatisticsRepository.save(oldPs);
-                }
-            }
-
-            if (previousDate != null) {
-                LocalDate prevDay = previousDate.toLocalDate();
-                StoreDailyStatistics oldDaily = storeDailyStatisticsRepository
-                        .findByStoreIdAndDate(previousStoreId, prevDay)
-                        .orElse(null);
-                if (oldDaily != null && oldDaily.getSent() > 0) {
-                    oldDaily.setSent(oldDaily.getSent() - 1);
-                    oldDaily.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                    storeDailyStatisticsRepository.save(oldDaily);
-                }
-
-                if (serviceType != PostalServiceType.UNKNOWN) {
-                    PostalServiceDailyStatistics oldPsDaily = postalServiceDailyStatisticsRepository
-                            .findByStoreIdAndPostalServiceTypeAndDate(previousStoreId, serviceType, prevDay)
-                            .orElse(null);
-                    if (oldPsDaily != null && oldPsDaily.getSent() > 0) {
-                        oldPsDaily.setSent(oldPsDaily.getSent() - 1);
-                        oldPsDaily.setUpdatedAt(ZonedDateTime.now(ZoneOffset.UTC));
-                        postalServiceDailyStatisticsRepository.save(oldPsDaily);
-                    }
-                }
-            }
-        }
+        trackStatisticsUpdater.updateStatistics(trackParcel, isNewParcel, previousStoreId, previousDate);
 
         // Обновляем историю доставки
         deliveryHistoryService.updateDeliveryHistory(trackParcel, oldStatus, newStatus, trackInfoListDTO);
