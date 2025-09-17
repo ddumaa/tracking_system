2d285e9d768124d1493a3137ead27206a880b08a
ddumaa <dimaduma62@gmail.com>
2025-09-17 18:20:33 +0300

    fix

--- src/main/java/com/project/tracking_system/service/telegram/BuyerTelegramBot.java
+++ src/main/java/com/project/tracking_system/service/telegram/BuyerTelegramBot.java
@@ -19,7 +19,7 @@ import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageRe
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Contact;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.api.objects.message.MaybeInaccessibleMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
