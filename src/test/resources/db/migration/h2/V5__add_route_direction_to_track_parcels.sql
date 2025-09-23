4deeb47157a3ce35081ebd64f974b3302ed02cbd
Dmitriy Anisimov <149701984+ddumaa@users.noreply.github.com>
2025-09-23 16:02:32 +0300

    Backfill route direction for return statuses

--- src/test/resources/db/migration/h2/V5__add_route_direction_to_track_parcels.sql
+++ src/test/resources/db/migration/h2/V5__add_route_direction_to_track_parcels.sql
@@ -1,5 +1,9 @@
 ALTER TABLE tb_track_parcels
     ADD COLUMN route_direction VARCHAR(32) DEFAULT 'TO_CUSTOMER' NOT NULL;
 
+UPDATE tb_track_parcels
+SET route_direction = 'TO_STORE'
+WHERE status IN ('RETURN_IN_PROGRESS', 'RETURN_PENDING_PICKUP', 'RETURNED');
+
 CREATE INDEX idx_track_parcels_route_direction
     ON tb_track_parcels(route_direction);
